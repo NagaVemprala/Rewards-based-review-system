5070328d6a0c21cef1a113ed1099dec3404c9f6a
NagaVemprala <Naga.Vemprala@gmail.com>
2020-03-27 11:13:42 -0500

    Update ecommerce.js

--- ethereum/ecommerce.js
+++ ethereum/ecommerce.js
@@ -3,7 +3,7 @@ import compiledFactory from './build/allProducts.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(compiledFactory.interface),
-  '0x7cB2B3DFA861C494573a73dfC3EBbFa8dB933360'
+  '0xFA74CC2fb9e1FF3796C58B06d0b960bD6dF66509'
 );
 
 export default instance;
